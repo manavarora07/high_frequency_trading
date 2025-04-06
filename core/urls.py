91d503de47781cbe68b82772a0fe88a3cf9b7413
Manav Arora <104390963+manavarora07@users.noreply.github.com>
2025-04-07 02:59:58 +0530

    urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from core.views import publish_trade
 from . import views
 
 urlpatterns = [
     path('', views.plot, name='plot'),
     path('plot-data/', views.trade_plot_data, name='plot-data'),
+    path('publish/', publish_trade, name='publish_trade'),
 ]
